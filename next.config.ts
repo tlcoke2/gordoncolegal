0a3f30a5ab3452ba6c8faab800ea1acd15861e81
tlcoke2 <tlcoke@hotmail.com>
2025-09-18 02:23:28 +0100

    Setup for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/gordoncolegal' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/gordoncolegal/' : '',
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
